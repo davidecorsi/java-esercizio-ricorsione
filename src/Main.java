291f7f91b949a50287f1d79538056900920a8078
Davide Corsi <davide.corsi@par-tec.it>
2021-10-01 15:58:33 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/*
+ * Scrivete un applicazione che calcoli se una stringa è palindroma o meno utilizzando un metodo ricorsivo.
+ */
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
